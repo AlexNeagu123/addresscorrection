2b7b4640ad143f32aa5de5e8bbaff76d7f8ae8b7
Alex Neagu <nalex4080@gmail.com>
2023-05-19 22:39:14 +0300

    Project Restructured. There the data parser and the web server are two separate applications now

---
+++ WebServer/src/main/java/com/pa/dto/AddressDTO.java
@@ -0,0 +1,12 @@
+package com.pa.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressDTO {
+    public String address;
+}
